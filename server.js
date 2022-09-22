7ef83a2b2dbb0b6f0dc3b47bc60e9065fe634ecd
Mark Nakayama <mark.nakay@gmail.com>
2022-09-22 15:15:35 -0600

    update rollbar

--- server.js
+++ server.js
@@ -43,13 +43,13 @@ app.post('/api/students', (req, res) => {
            rollbar.log('Tea name was added')
            res.status(200).send(students)
        }  else if (name === ''){
-           rollbar.critical('No tea name was typed')
+           Rollbar.critical('No tea name was typed')
            res.status(400).send('You must enter a name.')
     //    }  else if (name > max_chars){
     //        rollbar.critical('tea name is more than 24 characters')
     //        res.status(400).send('Name is too long.')
        }  else {
-           rollbar.warning('Tea name is already on the list')
+           Rollbar.warning('Tea name is already on the list')
            res.status(400).send('That tea already exists.')
        }
    } catch (err) {
